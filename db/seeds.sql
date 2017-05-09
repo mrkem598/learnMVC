11a56eb833ad52e2ac71083990ef3dca660e96de
Moh <mohakemal9@gmail.com>
2017-05-09 00:53:23 -0400

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,6 @@
-﻿
+INSERT INTO cats (name) VALUES ('Meeses');
+INSERT INTO cats (name) VALUES ('Bobbi');
+INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
+INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
+INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
+INSERT INTO cats (name) VALUES ('Owens');
