e606d09fc25bcb40cbd550c543c516dc70b93a51
Moh <mohakemal9@gmail.com>
2017-05-09 00:51:04 -0400

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,10 @@
-﻿
+### Schema
+CREATE DATABASE cat_db;
+USE cat_db;
+CREATE TABLE cats
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	sleepy BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
